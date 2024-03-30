0bcac2a52a7efe544035dee96f55c5de7ff0eea6
ulfhiraa <studyiraa@gmail.com>
2024-03-30 16:04:00 +0700

    first commit

---
+++ exercise.js
@@ -0,0 +1,12 @@
+function showAlert(){
+    alert(Date(), show(), show1())
+    Date()
+
+    function show(){
+        alert("Male & Female leadnya cinlok & nikah!!! tapi sekarang udah cerai :(, SAD. ((curhat))")
+    }
+
+    function show1(){
+        alert("PLIS INI WAJIB BANGET DITONTON. DraKor lawas yang minimal setahun sekali aku rewatch, SE-SERU ITU!")
+    }
+}
